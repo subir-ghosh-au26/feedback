211853baed0f69e14fe600c668e7d8048c6b850d
subir-ghosh-au26 <85132702+subir-ghosh-au26@users.noreply.github.com>
2024-12-31 13:38:25 +0530

    Update AdminPanel.js

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -34,7 +34,7 @@ const AdminPanel = () => {
 
  const fetchFeedbackData = async () => {
    try {
-     const response = await axios.get("/api/feedback");
+     const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/feedback`);
      setFeedbackData(response.data);
      setFilteredFeedback(response.data);
    } catch (error) {
